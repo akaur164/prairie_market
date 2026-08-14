7261bc5979083511a0eb329788a8a0dd7a7dd36e
Arshpreet <akUR154@rrc.ca>
2026-08-14 03:13:47 -0500

    Fix orders controller test authentication

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
 
 class OrdersControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @customer = customers(:one)
+    sign_in @customer
+  end
+
   test "should get index" do
     get orders_index_url
     assert_response :success
