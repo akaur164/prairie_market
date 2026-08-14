b5c09514d355534cd24660449aaf28cd28be18db
Arshpreet <akUR154@rrc.ca>
2026-08-14 01:33:39 -0500

    Add validations for admin product category and customer models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,16 @@
 class Product < ApplicationRecord
   belongs_to :category
+
+  has_many :order_items, dependent: :restrict_with_error
+
   has_one_attached :image
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price,
+            presence: true,
+            numericality: { greater_than_or_equal_to: 0 }
+  validates :category, presence: true
 
   def self.ransackable_attributes(_auth_object = nil)
     [
@@ -15,6 +25,9 @@ class Product < ApplicationRecord
   end
 
   def self.ransackable_associations(_auth_object = nil)
-    ["category"]
+    [
+      "category",
+      "order_items"
+    ]
   end
 end
